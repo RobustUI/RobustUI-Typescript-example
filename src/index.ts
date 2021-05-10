e3670829e154a2ecfbcd794be80855743446adcc
Mikkel Pedersen <mikkel.pedersen96@live.dk>
2021-05-10 13:24:21 +0200

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,83 +1,117 @@
-/*
-const inputField = document.getElementById("input") as HTMLInputElement;
-const lock = document.getElementById("lock");
-const faq = document.getElementById("FAQ-DIV");
-const movie = document.getElementById("MOVIE-DIV");
-const toggleSelective = new useCaseThree();
+import {usecasefour} from "./components/usecasefour";
 
-toggleSelective.sendInput('stream');
+const inputField = document.getElementById("input") as HTMLInputElement;
+
+const dropdown = document.getElementById("dropdown");
+const dropdownEl1 = document.getElementById("dropdownEl1");
+const dropdownEl2 = document.getElementById("dropdownEl2");
+const dropdownEl3 = document.getElementById("dropdownEl3");
+
+const lock = document.getElementById("lock");
+const toggleSelective = new usecasefour();
+
+toggleSelective.sendInput('stream', 0);
 inputField.onchange = (event) => {
     toggleSelective.sendInput('stream', +inputField.value);
 }
 
-toggleSelective.onNewConfiguration().subscribe(configurations => {
+toggleSelective.onNewConfiguration("dropdown").subscribe(configurations => {
     console.log(configurations);
     configurations.forEach(config => {
-        if (config.machine === "toggle") {
-            if (config.state === "on") {
-                lock.classList.add('unlocked');
-            } else {
-                lock.classList.remove('unlocked');
-            }
+        if (config.state === "show") {
+            document.getElementById("myDropdown").classList.add("show")
+        } else if (config.state === "notShow") {
+            document.getElementById("myDropdown").classList.remove("show")
         }
-        if (config.machine === "content") {
-            let internal = (config.state as Configuration[])[0]
-            if (internal.machine === 'movie') {
-                toggleSelective.unregisterElement(faq, "content::FAQ");
-                toggleSelective.registerElement(movie, "content::movie");
-                movie.style.display = "block"
-                faq.style.display = "none"
-            } else if (internal.machine === 'FAQ') {
-                toggleSelective.unregisterElement(movie, "content::movie");
-                toggleSelective.registerElement(faq, "content::FAQ");
-                movie.style.display = "none"
-                faq.style.display = "block"
-            }
-        }
-
     })
 })
- */
 
-/*
-import {ToggleSelective} from "./components/testSoem";
-import {Toggle} from "./components/Toggle";
+toggleSelective.onNewConfiguration("dropdown::toggle").subscribe(configurations => {
+    console.log(configurations);
+    configurations.forEach(config => {
+        if (config.state === "show") {
+            document.getElementById("myDropdown").classList.add("show")
+        } else if (config.state === "notShow") {
+            document.getElementById("myDropdown").classList.remove("show")
+        }
+        /*if (config.machine === "history") {
+            handleDropDownMenuElement(dropdownEl1, config.state as string);
+        } else if (config.machine === "contact") {
+            handleDropDownMenuElement(dropdownEl2, config.state as string);
+        } else if (config.machine === "jobs") {
+            handleDropDownMenuElement(dropdownEl3, config.state as string);
+        } */
+    })
+})
+toggleSelective.onNewConfiguration("dropdown::contact").subscribe(configurations => {
+    console.log(configurations);
 
-const lock = document.getElementById("lock");
-const faq = document.getElementById("FAQ-DIV");
-const movie = document.getElementById("MOVIE-DIV");
-const q1 = document.getElementById("Q1");
-const q2 = document.getElementById("Q2");
-const q3 = document.getElementById("Q3");
+    configurations.forEach(config => {
+        if (config.state === "show") {
+            document.getElementById("myDropdown").classList.add("show")
+        } else if (config.state === "notShow") {
+            document.getElementById("myDropdown").classList.remove("show")
+        }
+    })
+})
+toggleSelective.onNewConfiguration("dropdown::history").subscribe(configurations => {
+    console.log(configurations);
 
-const toggle = new Toggle("lock");
+    configurations.forEach(config => {
+        if (config.state === "show") {
+            document.getElementById("myDropdown").classList.add("show")
+        } else if (config.state === "notShow") {
+            document.getElementById("myDropdown").classList.remove("show")
+        }
+    })
+})
+toggleSelective.onNewConfiguration("dropdown::jobs").subscribe(configurations => {
+    console.log(configurations);
 
-toggle.registerElement(lock);
-toggle.onNewConfiguration().subscribe(configurations => {
+    configurations.forEach(config => {
+        if (config.state === "show") {
+            document.getElementById("myDropdown").classList.add("show")
+        } else if (config.state === "notShow") {
+            document.getElementById("myDropdown").classList.remove("show")
+        }
+    })
+})
+
+toggleSelective.onNewConfiguration("FAQ").subscribe(configurations => {
+    console.log(configurations);
     configurations.forEach(config => {
         if (config.state === "on") {
             lock.classList.add('unlocked');
-            toggleSelective.sendInput('stream', 0);
         } else {
             lock.classList.remove('unlocked');
-            toggleSelective.sendInput('stream', 1);
         }
     })
 })
 
-const toggleSelective = new ToggleSelective();
+function handleDropDownMenuElement(element: HTMLElement, state: string) {
+    if (state === "notSelected") {
+        element.classList.remove("hovered")
+    } else if (state === "Hovered") {
+        element.classList.add("hovered")
+    }
+}
 
 toggleSelective.onMachineSwitch.subscribe(machine => {
-    if (machine === 'movie') {
-        toggleSelective.unregisterElement(faq, "FAQ");
-        toggleSelective.registerElement(movie, "movie");
-        movie.style.display = "block"
-        faq.style.display = "none"
-    } else if (machine === 'FAQ') {
-        toggleSelective.unregisterElement(movie, "movie");
-        toggleSelective.registerElement(faq, "FAQ");
-        movie.style.display = "none"
-        faq.style.display = "block"
+    if (machine === "dropdown") {
+        toggleSelective.registerElement(dropdown, "dropdown::toggle");
+        toggleSelective.registerElement(dropdownEl1, "dropdown::history");
+        toggleSelective.registerElement(dropdownEl2, "dropdown::contact");
+        toggleSelective.registerElement(dropdownEl3, "dropdown::jobs");
+        toggleSelective.unregisterElement(lock, "FAQ");
+        dropdown.style.display = "block"
+        lock.style.display = "none"
+    } else if (machine === "FAQ") {
+        toggleSelective.unregisterElement(dropdown, "dropdown::toggle");
+        toggleSelective.unregisterElement(dropdownEl1, "dropdown::history");
+        toggleSelective.unregisterElement(dropdownEl2, "dropdown::contact");
+        toggleSelective.unregisterElement(dropdownEl3, "dropdown::jobs");
+        toggleSelective.registerElement(lock, "FAQ");
+        dropdown.style.display = "none"
+        lock.style.display = "block"
     }
 })
- */
